dd1f3fe0890fc20899a737e603fa2ec4605241ae
Valentyn Dorosh <wh2ts0n9057@gmail.com>
2018-08-06 14:27:45 +0300

    update

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -1,15 +1,34 @@
-import {BODY, ACTIVE} from '../constants';
+// import {BODY, ACTIVE} from '../constants';
 
-$('.js-dropdown').each((i, container) => {
+// $('.js-dropdown').each((i, container) => {
+//   container = $(container);
+//   const control = container.find('.js-dropdown-control');
+//   const items = container.find('.js-dropdown-item');
+//   control.on('click', e => {
+//    container.toggleClass(ACTIVE);
+//   });
+// });
+
+// $(document).on('click', e => {
+//   if ($(e.target).closest('.js-dropdown').length) return;
+//   $('.js-dropdown').removeClass(ACTIVE);
+// });
+
+$('[data-choose]').each((i, container) => {
   container = $(container);
-  const control = container.find('.js-dropdown-control');
-  const items = container.find('.js-dropdown-item');
-  control.on('click', e => {
-  	container.toggleClass(ACTIVE);
-  });
-});
+  const all = container.find('[data-choose-all]');
+  const items = container.find('[data-choose-item]');
 
-$(document).on('click', e => {
-  if ($(e.target).closest('.js-dropdown').length) return;
-  $('.js-dropdown').removeClass(ACTIVE);
+  
+  items.on('change', e => {
+    let allChecked = true;
+    items.each((i, item) => { if (!$(item).prop('checked')) allChecked = false; });
+    all.prop('checked', allChecked);
+  });
+
+  all.on('change', e => {
+    let allChecked = true;
+    items.each((i, item) => { if (!$(item).prop('checked')) allChecked = false; });
+    allChecked ? items.prop('checked', false) : items.prop('checked', true);
+  });
 });
